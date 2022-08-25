f5df198494953a469b5a5ad11d5802e9564d46d3
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-08-25 10:28:15 -0300

    Initial files

---
+++ src/libs/firebase.ts
@@ -0,0 +1,10 @@
+import {} from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyANDhQ-VnPclBr-j_rK_Ayc9O7Fvjz8fRc",
+  authDomain: "gallery-project-212f4.firebaseapp.com",
+  projectId: "gallery-project-212f4",
+  storageBucket: "gallery-project-212f4.appspot.com",
+  messagingSenderId: "203142445345",
+  appId: "1:203142445345:web:c1b2c2fee59fde61906660",
+};
